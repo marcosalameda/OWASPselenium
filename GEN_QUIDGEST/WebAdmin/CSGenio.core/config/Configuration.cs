bf0c0fa6f2ac905b86cda465ae34fb88c122cd43
quidgest <quidgest@users.noreply.quidgest.pt>
2024-06-06 15:05:45 +0100

    Build version: 2699 | Genio version: 345.35 | Database version: 3647 | Platform: MVC

--- GEN_QUIDGEST/WebAdmin/CSGenio.core/config/Configuration.cs
+++ GEN_QUIDGEST/WebAdmin/CSGenio.core/config/Configuration.cs
@@ -109,7 +109,7 @@ namespace CSGenio.framework
         /// <summary>
         /// Solution build version
         /// </summary>
-        public const int BuildVersionGen = 2697;
+        public const int BuildVersionGen = 2698;
         /// <summary>
         /// Solution release version
         /// </summary>

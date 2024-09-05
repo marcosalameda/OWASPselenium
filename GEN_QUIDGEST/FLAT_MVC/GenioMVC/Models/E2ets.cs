1ba707e36a400608ca1b928e61ea7ea5ce4906c0
Quidgest <quidgest@users.noreply.quidgest.pt>
2024-09-05 12:54:41 +0100

    Build version: 2783 | Genio version: 349.31 | Database version: 3754 | Platform: VUE

---
+++ GEN_QUIDGEST/FLAT_MVC/GenioMVC/Models/E2ets.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+using CSGenio.business;
+using CSGenio.framework;
+using CSGenio.persistence;
+using GenioMVC.Helpers;
+using GenioMVC.Models.Navigation;
+using Quidgest.Persistence;
+using Quidgest.Persistence.GenericQuery;
+
+using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
+using SelectList = System.Web.Mvc.SelectList;
+
+namespace GenioMVC.Models
+{
+	public class E2ets : ModelBase
+	{
+		[JsonIgnore]
+		public CSGenioAe2ets klass { get { return baseklass as CSGenioAe2ets; } set { baseklass = value; } }
+
+		private Glob _globTable;
+		/// <summary>
+		/// Gets a reference to the GLOB table
+		/// to provide access to the necessary fields
+		/// to client and server-side formulas.
+		/// </summary>
+		[JsonIgnore]
+		public virtual Glob TGlob { get { if (_globTable == null) _globTable = Glob.GetGlob(false, this?._fieldsToSerialize); return _globTable; } }
+
+		[DisplayName("ZZSTATE")]
+		/// <summary>Field : "ZZSTATE" Type: "INT" Formula:  ""</summary>
+		public int ValZzstate { get { return klass.ValZzstate; } set { klass.ValZzstate = value; } }
+		public bool ShouldSerializeValZzstate () => this.SerializeAllFields || this.FieldsToSerialize.Contains("E2ets.ValZzstate");
+
+		public E2ets() : this(UserContext.Current.User) { }
+
+		public E2ets(User u)
+		{
+			this.klass = new CSGenioAe2ets(u);
+		}
+
+		public E2ets(string[] fieldsToSerialize) : this()
+		{
+			SetFieldsToSerialize(fieldsToSerialize);
+		}
+
+		public E2ets(bool isEmpty, string[] fieldsToSerialize) : this(fieldsToSerialize)
+		{
+			this.isEmptyModel = isEmpty;
+		}
+
+		public E2ets(bool isEmpty) : this(isEmpty, null) { }
+
+		public E2ets(CSGenioAe2ets val, string[] fieldsToSerialize)
+		{
+			klass = val; SetFieldsToSerialize(fieldsToSerialize);
+		}
+
+		public E2ets(CSGenioAe2ets val) : this(val, null) { }
+
+		public E2ets(CSGenioAe2ets val, bool fillAreasRel, string[] fieldsToSerialize)
+		{
+			klass = val;
+			SetFieldsToSerialize(fieldsToSerialize);
+			FillRelatedAreas(val);
+		}
+
+		public E2ets(CSGenioAe2ets val, bool fillAreasRel) : this(val, fillAreasRel, null) { }
+
+		public void FillRelatedAreas(CSGenioAe2ets csgenioa)
+		{
+			if (csgenioa == null)
+				return;
+
+			foreach (RequestedField Qfield in csgenioa.Fields.Values)
+			{
+				switch (Qfield.Area)
+				{
+					default:
+						break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Search the row by key.
+		/// </summary>
+		/// <param name="id">The primary key.</param>
+		/// <param name="identifier">The identifier.</param>
+		/// <param name="fieldsToSerialize">The fields to serialize.</param>
+		/// <param name="fieldsToQuery">The fields to query.</param>
+		/// <returns>Model or NULL</returns>
+		public static E2ets Find(string id, string identifier = null, string[] fieldsToSerialize = null, string[] fieldsToQuery = null)
+		{
+			return Find(id, UserContext.Current, identifier, fieldsToSerialize, fieldsToQuery);
+		}
+
+		/// <summary>
+		/// Search the row by key.
+		/// </summary>
+		/// <param name="id">The primary key.</param>
+		/// <param name="userCtx">The user context.</param>
+		/// <param name="identifier">The identifier.</param>
+		/// <param name="fieldsToSerialize">The fields to serialize.</param>
+		/// <param name="fieldsToQuery">The fields to query.</param>
+		/// <returns>Model or NULL</returns>
+		public static E2ets Find(string id, UserContext userCtx, string identifier = null, string[] fieldsToSerialize = null, string[] fieldsToQuery = null)
+		{
+			var record = Find<CSGenioAe2ets>(id, userCtx, identifier, fieldsToQuery);
+			return record == null ? null : new E2ets(record, fieldsToSerialize) { Identifier = identifier };
+		}
+
+		[Obsolete("This method should never be used because it uses the default pagination but never allow in the parameters to set any other page. " +
+			"Might induce error in manual routines expection to return all the records in the database, when in fact they only get 1 page of records.")]
+		public static List<E2ets> Where(CriteriaSet args = null, string identifier = null, bool noLock = true)
+		{
+			return Where<CSGenioAe2ets>(false, args, null, 0, 0, null, identifier,noLock).RowsForViewModel<E2ets>();
+		}
+
+		[Obsolete("Please use the ModelBase<A>.Where method instead")]
+		public static ListingMVC<CSGenioAe2ets> Where(bool distinct, CriteriaSet args = null, Quidgest.Persistence.FieldRef[] fields = null, int offset = 0, int numRegs = 0, List<ColumnSort> sorts = null, string identifier = null, bool noLock = true, bool getTotal = false, string selectrow = "", CriteriaSet PagingPosEPHs = null, Quidgest.Persistence.FieldRef firstVisibleColumn = null)
+		{
+			return ModelBase.Where<CSGenioAe2ets>(distinct, args, fields, offset, numRegs, sorts, identifier, noLock, getTotal, selectrow, PagingPosEPHs, firstVisibleColumn);
+		}
+
+		[Obsolete("Please use the ModelBase<A>.All method instead")]
+		public static ListingMVC<CSGenioAe2ets> All(CriteriaSet args = null)
+		{
+			return Where<CSGenioAe2ets>(false, args, numRegs: -1);
+		}
+
+		public static List<E2ets> AllModel(CriteriaSet args = null, string identifier = null)
+		{
+			return Where<CSGenioAe2ets>(false, args, numRegs: -1, identifier: identifier).RowsForViewModel<E2ets>((r) => new E2ets(r));
+		}
+
+// USE /[MANUAL GQT MODEL E2ETS]/
+	}
+}

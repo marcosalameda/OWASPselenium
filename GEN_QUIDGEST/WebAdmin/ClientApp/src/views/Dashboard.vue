81bd75699e71b24926aba4733f476e673bc31e49
DBTestUser <dbtestuser@users.noreply.quidgest.pt>
2025-04-10 17:26:55 +0100

    Build version: 2925 | Genio version: 368.21 | Database version: 4006 | Platform: MVC

--- GEN_QUIDGEST/WebAdmin/ClientApp/src/views/Dashboard.vue
+++ GEN_QUIDGEST/WebAdmin/ClientApp/src/views/Dashboard.vue
@@ -64,7 +64,7 @@
 					</dl>
 					<dl class="row">
 						<dt>{{ Resources.VERSAO_DE_SISTEMA07287 }}</dt>
-						<dd>2924</dd>
+						<dd>2925</dd>
 						<dt>{{ Resources.VERSAO_DE_BASE_DE_DA46937 }}</dt>
 						<dd>{{ Model.VersionDbGen }}</dd>
 						<dt>{{ Resources.APP_MIGRATION_VERSIO41495 }}</dt>
@@ -72,9 +72,9 @@
 						<dt>{{ Resources.VERSAO_DOS_INDICES49454 }}</dt>
 						<dd>{{ Model.VersionIdxDbGen }}</dd>
 						<dt>{{ Resources.VERSAO_DE_GENIO44840 }}</dt>
-						<dd>366.23</dd>
+						<dd>368.21</dd>
 						<dt>{{ Resources.GERADO_EM27272 }}</dt>
-						<dd>04/03/2025</dd>
+						<dd>04/10/2025</dd>
 					</dl>
 					<dl class="row">
 						<span class="app-brand">

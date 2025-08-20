ac2e14220a09cb34f95c4a5de18f843596ca2baf
DBTestUser <dbtestuser@users.noreply.quidgest.pt>
2025-08-20 10:09:28 +0100

    Build version: 2967 | Genio version: 372.25 | Database version: 4171 | Platform: MVC

--- GEN_QUIDGEST/GQT/CSGenio.core/config/Configuration.cs
+++ GEN_QUIDGEST/GQT/CSGenio.core/config/Configuration.cs
@@ -110,7 +110,7 @@ namespace CSGenio.framework
         /// <summary>
         /// Solution build version
         /// </summary>
-        public const int BuildVersionGen = 2966;
+        public const int BuildVersionGen = 2967;
         /// <summary>
         /// Solution release version
         /// </summary>

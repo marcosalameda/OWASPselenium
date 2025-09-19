e23f96c1117172cb03292746c2b924972ac49964
DBTestUser <dbtestuser@users.noreply.quidgest.pt>
2025-09-19 11:20:47 +0100

    Build version: 2954 | Genio version: 372.05 | Database version: 4123 | Platform: VUE

--- GEN_QUIDGEST/GQT/CSGenio.core/config/Configuration.cs
+++ GEN_QUIDGEST/GQT/CSGenio.core/config/Configuration.cs
@@ -53,7 +53,7 @@ namespace CSGenio.framework
         /// <summary>
         /// Application version
         /// </summary>
-        public static int Version { get; } = 4203;
+        public static int Version { get; } = 4211;
 
         /// <summary>
         /// System id
@@ -88,12 +88,12 @@ namespace CSGenio.framework
         /// <summary>
         /// Version of the database
         /// </summary>
-        public const int VersionDbGen = 4203;
+        public const int VersionDbGen = 4211;
 
         /// <summary>
         /// Version of the database indexes
         /// </summary>
-        public const int VersionIdxDbGen = 1875;
+        public const int VersionIdxDbGen = 1883;
 
         /// <summary>
         /// Version of the latest upgrade index version
@@ -108,12 +108,12 @@ namespace CSGenio.framework
         /// <summary>
         /// Genio generator version
         /// </summary>
-        public const string GenioVersion = "373.41";
+        public const string GenioVersion = "372.05";
 
         /// <summary>
         /// Solution build version
         /// </summary>
-        public const int BuildVersionGen = 2955;
+        public const int BuildVersionGen = 2954;
 
         /// <summary>
         /// Solution release version

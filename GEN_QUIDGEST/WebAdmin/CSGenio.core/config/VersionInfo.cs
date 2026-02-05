c30fb57f73e41062a499c25f5d4c2961c8bed723
DBTestUser <dbtestuser@users.noreply.quidgest.pt>
2026-02-05 09:27:37 +0000

    Build version: 3000 | Genio version: 376.60 | Database version: 4446 | Platform: MVC

--- GEN_QUIDGEST/WebAdmin/CSGenio.core/config/VersionInfo.cs
+++ GEN_QUIDGEST/WebAdmin/CSGenio.core/config/VersionInfo.cs
@@ -45,7 +45,7 @@ namespace CSGenio.framework
         /// <summary>
         /// Generated version. Is incremented each time there is a generation
         /// </summary>
-        public const int Generation = 2999;
+        public const int Generation = 3000;
 
 
         /// <summary>
@@ -93,7 +93,7 @@ namespace CSGenio.framework
         /// <summary>
         /// Date when the code was generated
         /// </summary>
-        public static readonly DateTime GenerationDate = new DateTime(2026, 2, 4);
+        public static readonly DateTime GenerationDate = new DateTime(2026, 2, 5);
 
     }
 }
